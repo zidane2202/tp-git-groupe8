5167c40911682b201afc792830a02bdc6e0f6057
Zidane <pythiemorne22@gmail.com>
2025-10-25 19:44:47 +0100

    resolution de merge confilcts

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,32 +1,39 @@
 module.exports = {
   extends: "stylelint-config-standard",
   rules: {
-    // Ignorer les doublons de propriétés
+    // Désactiver certaines règles globalement
     "declaration-block-no-duplicate-properties": null,
-    // Ignorer les sélecteurs dupliqués
     "no-duplicate-selectors": null,
-    // Ignorer les erreurs sur box-shadow, transform, transition
     "property-no-unknown": null,
     "declaration-property-value-no-unknown": null,
     "font-family-no-missing-generic-family-keyword": null,
     "declaration-block-no-shorthand-property-overrides": null,
     "property-no-vendor-prefix": null,
     "no-empty-source": null,
-     "declaration-block-no-duplicate-properties": null,
-    "declaration-block-no-shorthand-property-overrides": null,
-    "declaration-property-value-no-unknown": null,
     "property-no-deprecated": null,
-    "font-family-no-missing-generic-family-keyword": null,
-    "no-empty-source": null,
     "media-feature-name-no-unknown": null,
     "no-descending-specificity": null,
     "declaration-block-single-line-max-declarations": null,
-      "selector-pseudo-element-no-unknown": null,
+    "selector-pseudo-element-no-unknown": null,
     "selector-pseudo-class-no-unknown": null,
-    "selector-class-pattern": null
+    "selector-class-pattern": null,
+    "tagname-lowercase": null,
+    "spec-char-escape": null,
+    "attr-no-duplication": null,
+    "selector-attribute-quotes": null,
+    "rule-empty-line-before": null,
+    "color-function-notation": null,
+    "color-hex-length": null,
+    "value-no-vendor-prefix": null,
+    "selector-pseudo-element-colon-notation": null
   },
   ignoreFiles: [
-    "fonts/**", // Ignorer tous les fichiers fonts
-    "css/vendor/**" // Ignorer les fichiers vendors
+    "fonts/",                  // Ignorer tous les fichiers fonts
+    "css/vendor/",             // Ignorer tous les fichiers vendors
+    "css/aos.css",
+    "css/bootstrap-datepicker.css",
+    "css/style.css",
+    "css/_mixins.css",
+    "css/_site-blocks.css"
   ]
 };
